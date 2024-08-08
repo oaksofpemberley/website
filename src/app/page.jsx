d2bc17f4f862ba7f7f218de6d6e1df440b0b2460
oaksofpemberley <90154941+oaksofpemberley@users.noreply.github.com>
2024-08-08 17:09:14 -0500

    text change test
    text change test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -260,7 +260,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            Thought leader, founder, and amateur astronaut.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Spencer, a software designer and entrepreneur based in New York
