f45417d06134a5a86404121d455ed2745d922d29
oaksofpemberley <90154941+oaksofpemberley@users.noreply.github.com>
2024-08-08 19:08:33 -0500

    Name change
    Name change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -263,7 +263,7 @@ export default async function Home() {
             Thought leader, founder, and amateur astronaut.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
+            I’m Ron, a software designer and entrepreneur based in New York
             City. I’m the founder and CEO of Planetaria, where we develop
             technologies that empower regular people to explore space on their
             own terms.
