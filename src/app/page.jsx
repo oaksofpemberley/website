87db6b1a48508e67fd307110b7dbba62d2cc0684
oaksofpemberley <90154941+oaksofpemberley@users.noreply.github.com>
2024-08-09 16:14:35 -0500

    removed government
    removed government

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -255,7 +255,7 @@ export default async function Home() {
             Technologist, Thinker, Doer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Ron, an experienced government computer engineer, acquisitions, staff and data officer. I find fulfillment in helping people succeed. I’m the founder and CEO of Track-Record.co, an LLC that works with
+            I’m Ron, a computer engineer, acquisitions, staff and data officer. I find fulfillment in helping people succeed. I’m the founder and CEO of Track-Record.co, an LLC that works with
             bleeding edge technologies to empower commercial, Department of Defense and Federal organizations to push the boundaries of what is possible.
           </p>
           <div className="mt-6 flex gap-6">
