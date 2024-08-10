d5f8267362b190e921a11ff26cbd73c3d4f826b7
oaksofpemberley <90154941+oaksofpemberley@users.noreply.github.com>
2024-08-10 15:52:36 -0500

    Menu Reorder
    Menu Reorder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,10 +22,11 @@ export function Footer() {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">Why</NavLink>
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/speaking">Speaking</NavLink>
                 <NavLink href="/customers">Customers</NavLink>
+                <NavLink href="/projects">Projects</NavLink>
+                <NavLink href="/articles">Articles</NavLink>
+                <NavLink href="/speaking">Speaking</NavLink>
+                <NavLink href="/about">About</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Ron Synakowski. All rights
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -114,11 +114,12 @@ function MobileNavigation(props) {
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
             <MobileNavItem href="/why">Why</MobileNavItem>
-            <MobileNavItem href="/about">About</MobileNavItem>
-            <MobileNavItem href="/articles">Articles</MobileNavItem>
-            <MobileNavItem href="/projects">Projects</MobileNavItem>
-            <MobileNavItem href="/speaking">Speaking</MobileNavItem>
             <MobileNavItem href="/customers">Customers</MobileNavItem>
+            <MobileNavItem href="/projects">Projects</MobileNavItem>
+            <MobileNavItem href="/articles">Articles</MobileNavItem>
+            <MobileNavItem href="/speaking">Speaking</MobileNavItem>
+            <MobileNavItem href="/about">About</MobileNavItem>
+ 
           </ul>
         </nav>
       </PopoverPanel>
@@ -154,11 +155,12 @@ function DesktopNavigation(props) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/why">Why</NavItem>
-        <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
         <NavItem href="/customers">Customers</NavItem>
+        <NavItem href="/projects">Projects</NavItem>
+        <NavItem href="/articles">Articles</NavItem>
+        <NavItem href="/speaking">Speaking</NavItem>
+        <NavItem href="/about">About</NavItem>
+
       </ul>
     </nav>
   )
