0f49eb873e01d92fe479cb584a48a33d7c821cfe
oaksofpemberley <90154941+oaksofpemberley@users.noreply.github.com>
2025-02-12 14:06:37 -0600

    New LinkedIN Photo Link
    New LinkedIN Photo Link

--- src/app/speaking/page.jsx
+++ src/app/speaking/page.jsx
@@ -1,59 +1,27 @@
-import { Card } from '@/components/Card'
-import { Section } from '@/components/Section'
-import { SimpleLayout } from '@/components/SimpleLayout'
+// VideoBackground.js
 
-function SpeakingSection({ children, ...props }) {
+import React, { useRef, useEffect } from 'react';
+
+const VideoBackground = ({ videoUrl }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) { 
+      video.loop = true;
+      video.autoplay = true;
+      video.muted = true;
+      video.volume = 0;
+    }
+  }, );
+
   return (
-    <Section {...props}>
-      <div className="space-y-16">{children}</div>
-    </Section>
-  )
-}
-
-function Appearance({ title, description, event, cta, href }) {
-  return (
-    <Card as="article">
-      <Card.Title as="h3" href={href}>
-        {title}
-      </Card.Title>
-      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
-      <Card.Description>{description}</Card.Description>
-      <Card.Cta>{cta}</Card.Cta>
-    </Card>
-  )
-}
-
-export const metadata = {
-  title: 'Speaking',
-  description:
-    'I’ve spoken at events all around the world and been interviewed for many podcasts.',
-}
-
-export default function Speaking() {
-  return (
-    <SimpleLayout
-      title="I’ve spoken at events all around the world, at all levels, and been interviewed for numerous efforts."
-      intro="One of my favorite ways to share my ideas is visually."
-    >
-      <div className="space-y-20">
-        <SpeakingSection title="Conferences">
-          <Appearance
-            href="#"
-            title="GovExec"
-            description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
-            event="SysConf 2021"
-            cta="Watch video"
-          />
-          <Appearance
-            href="#"
-            title="AFTIC 2019: DAF Enterprise Data Strategy"
-            description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
-            event="Business of Startups 2020"
-            cta="Watch video"
-          />
-        </SpeakingSection>
-      
+    <div >
+      <video ref={videoRef} src={videoUrl}  />
+    </div>
+        <div>
+        <VideoBackground videoUrl="https://cdn.dribbble.com/userupload/11001831/file/original-ec7c060b81c016f968e7cc3bf103a6a0.mp4" />
+        {/* Your other page content */}
       </div>
-    </SimpleLayout>
-  )
-}
+  );
+};
